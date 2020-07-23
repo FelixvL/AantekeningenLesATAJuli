58f53f2026d4417cb10d83269f7a8e5e6a9244c4
FelixvL <digitaleservice@gmail.com>
2020-07-23 16:46:33 +0200

    array met forloop en string

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 class Demo {   // arrays
 	public static void main(String[] args) {
 		int[] getallen2 = {4,3,7,88,24,99, 9,7,99};
-		
+		Persoon[] personen = {new Persoon(38,"Piet"),new Persoon(28,"Marleen"),new Persoon(23,"Karin")};
 		for(int x = 0 ; x < 10 ; x++) {
 			System.out.println("hoi");
 			System.out.println(x);
@@ -14,6 +14,27 @@ class Demo {   // arrays
 			System.out.println(getallen2[y]);
 		}
 		
+		for(int r = 0 ; r < personen.length ; r++) {
+			System.out.println(personen[r].naam);
+		}
+		
+		String teraden = "allerleiletter";
+		for(int t = 0; t < teraden.length() ; t++) {
+			System.out.println(teraden.charAt(t));
+		}
+		
+		
+		
+		
 		
 	}	
+}
+
+class Persoon{
+	int leeftijd;
+	String naam;
+	Persoon(int leef, String nm){
+		leeftijd = leef;
+		naam = nm;
+	}
 }
